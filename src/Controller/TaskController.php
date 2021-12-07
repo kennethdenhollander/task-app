de46bf3f063859c8c8107ae4fb4bb1eb97e5ccb8
Kenneth den Hollander <80956108+kennethdenhollander@users.noreply.github.com>
2021-12-07 20:28:59 +0100

    Added tepmlating to application

--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -2,18 +2,18 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class TaskController
+class TaskController extends AbstractController
 {   
     #[Route('/tasks')]
     public function task(): Response
     {
         $number = random_int(0, 100);
-
-        return new Response (
-            $number
-        );
+        return $this->render('tasks/task.html.twig', [
+            'number' => $number,
+        ]);
     }
 }
