d99ba5b1db60d9e2439865d297e3d73df2d1ad81
Kenneth den Hollander <80956108+kennethdenhollander@users.noreply.github.com>
2021-12-07 21:04:06 +0100

    Added new routes

--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -8,11 +8,28 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class TaskController extends AbstractController
 {   
-    #[Route('/tasks')]
-    public function task(): Response
+    #[Route('/tasks', name: 'task_index')]
+    public function taskIndex(): Response
     {
         $number = random_int(0, 100);
-        return $this->render('tasks/task.html.twig', [
+        return $this->render('tasks/task-index.html.twig', [
+            'number' => $number,
+        ]);
+    }
+
+    #[Route('/tasks/{slug}', name: 'task_single')]
+    public function singleTask(string $slug): Response
+    {
+        return $this->render('tasks/task-single.html.twig', [
+            'slug' => $slug,
+        ]);
+    }
+
+    #[Route('/register-task', name: 'register_task')]
+    public function registerTask(): Response
+    {
+        $number = random_int(0, 100);
+        return $this->render('tasks/register-task.html.twig', [
             'number' => $number,
         ]);
     }
