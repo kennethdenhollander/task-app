e6bd575da85e0f8aef1b9d6e5141ce2863aef0ff
Kenneth den Hollander <80956108+kennethdenhollander@users.noreply.github.com>
2021-12-08 23:18:52 +0100

    Changed Task controller & View

---
+++ src/Controller/TasksController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class TasksController extends AbstractController
+{
+    #[Route('/tasks', name: 'tasks')]
+    public function index(): Response
+    {
+        $tasks = [
+            '1' => 'Applicatie maken',
+            '2' => 'Vaatwasser leeghalen',
+            '3' => 'Prullenbak legen'
+        ];
+
+        return $this->render('tasks/task-index.html.twig', [
+            'tasks' => $tasks
+        ]);
+    }
+}
