5c22ce15ec1359498b75d51f8b32d545b8c2351a
Kenneth den Hollander <80956108+kennethdenhollander@users.noreply.github.com>
2021-12-11 00:02:39 +0100

    Create on homepage update
    Updated the controller so the form renders on the page where all the tasks are loaded. This makes it easier to create task and view your list at the same time

--- src/Controller/TasksController.php
+++ src/Controller/TasksController.php
@@ -17,53 +17,12 @@ class TasksController extends AbstractController
     // https://symfony.com/doc/current/forms.html#rendering-forms
 
     #[Route('/', name: 'tasks')]
-    public function index(ManagerRegistry $doctrine): Response
+    public function index(ManagerRegistry $doctrine, Request $request): Response
     {
+        // Getting all tasks
         $tasks = $doctrine->getRepository(Task::class)->findAll();
-
-        return $this->render('tasks/task-index.html.twig', [
-            'tasks' => $tasks,
-        ]);
-    }
-
-    // Making a route where the information of task is shown
-    #[Route('/tasks/{id}', name: 'task_show')]
-    public function show(ManagerRegistry $doctrine, int $id): Response
-    {
-        $task = $doctrine->getRepository(Task::class)->find($id);
-
-        if (!$task) {
-            throw $this->createNotFoundException(
-                'No product found for id '.$id
-            );
-        }
-
-        return $this->render('tasks/task-single.html.twig', [
-            'task' => $task,
-        ]);
-    }
-
-    // Creating a route to create a task
-    #[Route('/register-task', name: 'create_task')]
-    public function createTask(ManagerRegistry $doctrine): Response
-    {
-        $entityManager = $doctrine->getManager();
-
-        // Input should be replaced with user input
-        $task = new Task();
-        $task->setName('Stofzuigen');
-        $task->setDescription('Woonkamer, Badkamer & Keuken');
-
-        $entityManager->persist($task);
-        $entityManager->flush();
-
-        return new RedirectResponse($this->generateUrl('tasks'));
-    }
-
-    // Page to create a task
-    #[Route('/create', name: 'create')]
-    public function create(Request $request, ManagerRegistry $doctrine): Response
-    {
+        
+        // Setting new Task object and creating form
         $task = new Task();
         $form = $this->createForm(TasksType::class, $task);
         $form->handleRequest($request);
@@ -82,8 +41,26 @@ class TasksController extends AbstractController
             return $this->redirectToRoute('tasks');
         }
 
-        return $this->renderForm('tasks/new.html.twig', [
-            'form' => $form,
+        return $this->render('tasks/index.html.twig', [
+            'tasks' => $tasks,
+            'form' => $form->createView()
+        ]);
+    }
+
+    // Making a route where the information of task is shown
+    #[Route('/tasks/{id}', name: 'task_show')]
+    public function show(ManagerRegistry $doctrine, int $id): Response
+    {
+        $task = $doctrine->getRepository(Task::class)->find($id);
+
+        if (!$task) {
+            throw $this->createNotFoundException(
+                'No product found for id '.$id
+            );
+        }
+
+        return $this->render('tasks/task-single.html.twig', [
+            'task' => $task,
         ]);
     }
 }
